1171269743f39c1dc0aeeae8a3b0d0d9b65782ac
Matheus G <gmatheus66@hotmail.com>
2019-04-03 11:18:09 -0300

    remoção da pasta projetopart1

--- projeto-part1/concluido.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $dado = $_GET['nome'] . ';' . $_GET['sobrenome'] . ';' . $_GET['idade'] . ';' .  $_GET['e-mail']. ';' .  $_GET ['senha'] . ';' .  $_GET['matricula'] . ';' . "\n";
-
-    $handle = fopen('dados.csv', 'a+');
-    fwrite($handle, $dado);
-    fclose($dado);
-?>

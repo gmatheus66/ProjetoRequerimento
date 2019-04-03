9c8d21282b4ca67867d3ccd6b5908b981d3e0b19
Matheus G <gmatheus66@hotmail.com>
2019-04-03 11:43:20 -0300

    mudança na extensao do arquivo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Ta com um css "clone" das outras temporariamente, quando tiver desenvolvendo um novo layout mudarei --> 
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>Inicial</title>
+</head>
+<body>
+	
+<div class="topo">
+    <a href="index.html" class="a1"> IFPE</a>
+    <br>
+    <a href="index.html" class="a2"> Início</a>
+    <a href="cadastro.php" class="a2"> Cadastro</a>
+    <a href="login.php" class="a2"> Login</a>
+    <a href="listagem.php" class="a2"> Listagem</a>
+</div>
+<p class="explica">
+Esta página tem como objetivo a solicitação de requerimentos institucionais do IFPE campos Igarassu.
+</p>
+ </div> 
+<div class="baixo"></div>
+</body>
+</html>

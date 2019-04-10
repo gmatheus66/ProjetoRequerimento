e3b1343561f5d3a241145e16eb3e4f9c1ae12a7d
layseduarda1 <layseduarda449@gmail.com>
2019-04-10 16:57:54 -0300

    implementamos o carousel

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <head>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
     <meta charset="UTF-8">
     <title>Inicial</title>
+
+
 </head>
 <body>
 
@@ -14,12 +21,43 @@
     <a href="cadastro.php" class="aMenu"> CADASTRO</a>
     <a href="login.php" class="aMenu"> LOGIN</a>
     <a href="cadastroADM.php" class="aMenu"> CADASTRO ADM </a>
+
+
 </div>
 
-<!--     <div>
-       <img align="left" src="imagens/logoifpe.png" width="10%">
-       <p align="left">Lorem Ipsum</p>
-    </div> -->
+<div class="container slide" style="width: 100%; padding: 0%;"> 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner">
+      <div class="item active">
+        <center><img src="imagens/narinas.jpeg" alt="Los Angeles" style="width:20%;"></center>
+      </div>
+
+      <div class="item">
+        <center><img src="imagens/desgraça.jpeg" alt="Chicago" style="width:20%;"></center>
+      </div>
+    
+      <div class="item">
+        <center><img src="imagens/tristeza.jpeg" alt="New york" style="width:20%;"></center>
+      </div>
+    </div>
+
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
 
 <!-- <div class="degrade">
 Esta página tem como objetivo a solicitação de requerimentos institucionais do IFPE campos Igarassu.

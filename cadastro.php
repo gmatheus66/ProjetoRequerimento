b2851bc34d52107d1e02c8884a8a85ca266475a8
juciele <juciele.bol@gmail.com>
2019-04-23 12:49:10 -0300

    completei o select

--- cadastro.php
+++ cadastro.php
@@ -5,24 +5,26 @@
     <title>Cadastro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/cadastro.css">
+    <script src="js/jquery-3.4.0.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
    <div class="banner"> 
-    <img class="imgBanner" src="imagens/banner.png">  
-    <a href="index.php" class="aMenu"> HOME</a>
-    <a href="cadastro.php" class="aMenu"> CADASTRO</a>
-    <a href="login.php" class="aMenu"> LOGIN</a>
+        <img class="imgBanner" src="imagens/banner.png">  
+        <a href="index.php" class="aMenu"> HOME</a>
+        <a href="cadastro.php" class="aMenu"> CADASTRO</a>
+        <a href="login.php" class="aMenu"> LOGIN</a>
     </div>
 
 <div class= "input">        
     <form class="form" action="login.php" method="POST">
-    <select id="Cadastro">
-    <option value="ALUNO">aluno</option>
-    <option value="ADM">admistração</option>
-    </select>
-
-    <ALUNO>
+        <select id="Cadastro" onchange="verif()">
+            <option value="aluno">aluno</option>
+            <option value="adm">admistração</option>
+        </select>
+        
+    <div class="_cadastro" id="">
         <label>Nome:</label>
         <input type="text" id="input" class="inp" name="nome" placeholder="Nome...">
         
@@ -46,53 +48,39 @@
 
         <label>Confirmar Senha:</label>
         <input type="password" class="inp" name="senhaConfir" placeholder="Confirmar Senha...">
-    </ALUNO>
 
-    <ADM>
-         <label>Nome:</label>
-        <input type="text" id="input" class="inp" name="nome" placeholder="Nome...">
-        
+        <label>CPF</label>
+        <input type="number" name="cpf" class="inp" placeholder="Insira seu CPF">
 
-        <label>Sobrenome:</label>
-        <input type="text" class="inp" name="sobrenome" placeholder="Sobrenome..." >
-      
-
-        <label>Idade:</label>
-        <input type="text" class="inp" name="idade"  placeholder="Idade..." maxlength="2" >
-        
-
-        <label>Cargo:</label>
-        <input type="text" class="inp" name="Cargo" placeholder="Cargo...">
-
+        <label>RG</label>
+        <input type="number" name="RG" class="inp" placeholder="Insira seu RG">
+    </div>
+    <div class="d-none" id ="adm">
+        <label>Cargo</label>
+        <input type="text" name="cargo" class="inp" placeholder="Insira seu Cargo">
 
         <label>Cadastro do Funcionario</label>
-        <input type="text" class="inp" name="Cadastro" placeholder="Cadastro..." maxlength="10" >
-
-
-        <label>E-mail:</label>
-        <input type="e-mail" class="inp" name="e-mail" placeholder="E-mail...">
-      
-      <label>CPF:</label>
-      <input type="text" id="input" class="inp" name="CPF" plceholder="CPF..." maxlength="11" >
-
-      <label>RG:</label>
-        <input type="text" id="input" class="inp" name="RG" placeholder="RG..." maxlength="9">
-
-
-        <label>Senha:</label>
-        <input type="password" class="inp" name="senha" placeholder="Senha...">
-  
-
-        <label>Confirmar Senha:</label>
-        <input type="password" class="inp" name="senhaConfir" placeholder="Confirmar Senha...">
-
-    </ADM>
+        <input type="text" name="cadFunc" class="inp" placeholder="Insira seu Cadastro">
+    </div>
      
      
         <button type="submit" class="buuton" onclick="alert('Login efetuado com sucesso!')">Enviar</button>
    </form>
-    
-</div> 
+    <script type="text/javascript">
+        var bool       = true;
 
+        function verif(){
+            var adm        = document.getElementById('adm');
+            var opt = document.getElementById('Cadastro').value;
+            console.log(opt);
+            if (opt == "adm") {
+                adm.className = "";
+            } else if (opt == "aluno") {
+                adm.className = "d-none";
+            }
+        }
+
+    </script>
+</div> 
 </body>
 </html>

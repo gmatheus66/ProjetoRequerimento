07037e1b0bff783767f503a74ba2fad36f7a81d2
filipeandrev <filipe.andrev7@gmail.com>
2019-05-09 21:50:33 -0300

    Adicionar o banner com botões e logo a página de requerimentos

--- requerimento.php
+++ requerimento.php
@@ -7,6 +7,20 @@
   <title>Requerimento</title>
 </head>
 <body>
+
+<div>
+  <a class="tilt" href="index.php"><img class="logoIF" src="imagens/logoIF.png"></a>
+  <span class="titleBanner"><a class="tilt" href="index.php"> Instituto Federal de Pernambuco</a></span>
+</div>
+<span class="titleBanner2">Requerimentos</span>
+<div class="banner"> 
+    <img class="imgBanner" src="imagens/banner.png">  
+    <span><a href="index.php" class="aMenu" > HOME</a></span>
+    <span><a href="cadastro.php" class="aMenu"> CADASTRO</a></span>
+    <span><a href="login.php" class="aMenu"> ENTRAR</a></span>
+    <span><a href="requerimento.php" class="aMenu"> REQUERIMENTO</a></span>
+</div>
+
   <div class="fieldset">
     <form method="POST" onclick="menu()">
       <fieldset class="topico">

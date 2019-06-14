62d1e28e25dd1b492c34518daa5a6336cb1e314e
GuiLira <liraguilherme252@gmail.com>
2019-06-13 23:15:18 -0300

    Ajustes, no segundo tipo de card.

--- status.php
+++ status.php
@@ -73,12 +73,16 @@ try{
         Featured
         </div>
         <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-success">Go somewhere</a>
+            <h5 class="card-title">Status</h5>
+            <p class="card-text"><?=$row['REQ_STATUS']?></p>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Descrição</h5>
+            <p class="card-text"><?=$row['REQ_DESCICAO']?></p>
+            <!-- <a href="#" class="btn btn-success">Go somewhere</a> -->
         </div>
         <div class="card-footer text-muted">
-            2 days ago
+            <?= $data["REQ_DT_ABERTURA"]?>
         </div>
     </div>
 

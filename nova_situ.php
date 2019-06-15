609f1c52271a0ec9e4a97cffbcc1d793d418e107
Murielson <jhonsnoow32@gmail.com>
2019-06-15 09:25:20 -0300

    Continuação da modificação do requerimento.

--- nova_situ.php
+++ nova_situ.php
@@ -1,3 +1,13 @@
-<?php  
+<?php
+include "phpBD/conect.php";
+include "init.php";
+include  "phpBD/func.php";
+
+$status = $_POST['status.php'];
+$obs = $_POST['obs'];
+
+$smtt = $con->prepare("SELECT  FROM heroku_70137967cfc9460.REQUERIMENTO");
+$smtt->execute();
+$func = $smtt->fecth();
 
 ?>

1ab8d029d7181c81ec7837b2d16f9d5c40a2eae3
Matheus G <gmatheus66@hotmail.com>
2019-06-17 11:51:33 -0300

    correcao do erro

--- phpBD/conect.php
+++ phpBD/conect.php
@@ -2,8 +2,8 @@
     global $con;
     
     try{
-       //$con = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_70137967cfc9460;charset=utf8', 'b7c8700356c454', '814395c0');
-       $con = new PDO('mysql:host=localhost:3306;dbname=ProjetoRequerimento;charset=utf8', 'root', 'matheus0186');
+       $con = new PDO('mysql:host=us-cdbr-iron-east-02.cleardb.net;dbname=heroku_70137967cfc9460;charset=utf8', 'b7c8700356c454', '814395c0');
+       //$con = new PDO('mysql:host=localhost:3306;dbname=ProjetoRequerimento;charset=utf8', 'root', 'matheus0186');
         $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         echo 'ERROR!';

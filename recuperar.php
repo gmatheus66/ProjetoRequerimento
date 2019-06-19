d2e33f0572d6cda49263370a747219e7b4a9f955
jucielefernandes <juciele.bol@gmail.com>
2019-06-18 23:08:29 -0300

    criação de tokens

--- recuperar.php
+++ recuperar.php
@@ -9,17 +9,37 @@
 <div class="view" style="background-image: url('imagens/recuperar.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
 
 <h1>Recuperar Senha</h1>
-<br>
-<br>
-<a class= "r1">Informe o email do usuário</a>
 
+<a class= "r1">Informe o email do usuário</a>
 
 	<form action="recuperar.php">
 	<div class="inpu">
-
 	<input type="text" placeholder="E-mail...">
-	<br>
 	<button type="submit" class="buuton" onclick="alert('Enviado!') ">Enviar</button>
+	
+
+	<script>
+		 $email = isset($_POST["email"]) ? $_POST["email"] : "";
+
+
+	$consulta = mysql_query("SELECT * FROM cadastro WHERE email = '$email'");
+
+	if (mysql_num_rows($consulta == 0 )) {
+	echo "<script language='Javascript'>"
+			alert('Não encontramos o seu email na base de dados!');
+			location.href='recuperar.php';
+		}
+		/*
+
+ no corpo do email coloco o link para o usuario redefinir a senha 
+
+http://redefineasenha.com.br?key=$key
+função que gera o token = random bytes e bin2hex .
+$key = rand(); funcao parar gerar o numero token
+*/
+	</script>
+  
+
   </form>
 	</div> 
 </html>
